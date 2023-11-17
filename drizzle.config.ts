799bac6f546a7297a48f5a4ee7e10d11be9627d1
Shreyans Jain <shreyans@shreyans.sh>
2023-11-18 03:43:52 +0530

    try fixing bug x4
    Signed-off-by: Shreyans Jain <shreyans@shreyans.sh>

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from "drizzle-kit"
+// import { type Config } from "drizzle-kit"
 
-import { env } from "@/env.mjs"
+// import { env } from "@/env.mjs"
 
-export default {
-    schema: "./src/server/db/schema.ts",
-    driver: "mysql2",
-    dbCredentials: {
-        uri: env.DATABASE_URL,
-    },
-    tablesFilter: ["shreyans.sh_*"],
-} satisfies Config
+// export default {
+//     schema: "./src/server/db/schema.ts",
+//     driver: "mysql2",
+//     dbCredentials: {
+//         uri: env.DATABASE_URL,
+//     },
+//     tablesFilter: ["shreyans.sh_*"],
+// } satisfies Config
