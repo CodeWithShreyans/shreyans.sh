fc6f8b656e10744c703d317730840de3a1654686
Shreyans Jain <shreyans@shreyans.sh>
2024-01-06 15:13:02 +0530

    try x2
    Signed-off-by: Shreyans Jain <shreyans@shreyans.sh>

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,4 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files"
+import { defineDocumentType, makeSource } from "@contentlayer/source-files"
 import rehypeSlug from "rehype-slug"
 import remarkGfm from "remark-gfm"
 
