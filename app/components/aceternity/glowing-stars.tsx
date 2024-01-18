459745004db30542cca1c88e42eae674be992825
Shreyans Jain <shreyans@shreyans.sh>
2024-01-18 23:33:14 +0530

    improve stack card colors
    Signed-off-by: Shreyans Jain <shreyans@shreyans.sh>

--- app/components/aceternity/glowing-stars.tsx
+++ app/components/aceternity/glowing-stars.tsx
@@ -30,7 +30,7 @@ export const GlowingStarsBackgroundCard = ({
                 setMouseEnter(false)
             }}
             className={cn(
-                "bg-[linear-gradient(110deg,#333_0.6%,#222)] p-4 max-w-md max-h-[20rem] h-full w-full rounded-xl border border-[#eaeaea] dark:border-neutral-600",
+                "bg-secondary p-4 max-w-md max-h-[20rem] h-full w-full rounded-xl border border-[#eaeaea] dark:border-neutral-600",
                 className,
             )}
         >
@@ -50,7 +50,7 @@ export const GlowingStarsDescription = ({
     children?: React.ReactNode
 }) => {
     return (
-        <p className={cn("text-base text-white max-w-[16rem]", className)}>
+        <p className={cn("text-base text-primary max-w-[16rem]", className)}>
             {children}
         </p>
     )
@@ -64,7 +64,7 @@ export const GlowingStarsTitle = ({
     children?: React.ReactNode
 }) => {
     return (
-        <h2 className={cn("font-bold text-2xl text-[#eaeaea]", className)}>
+        <h2 className={cn("font-bold text-2xl text-primary/90", className)}>
             {children}
         </h2>
     )
