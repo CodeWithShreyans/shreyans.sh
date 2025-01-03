a8c1c185a921770f440df0d165053a24c04e0255
Shreyans Jain <shreyans@shreyans.sh>
2025-01-03 20:37:19 +0530

    layout and webhook scaffold
    Signed-off-by: Shreyans Jain <shreyans@shreyans.sh>

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,24 @@
-// @ts-check
+//
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "astro/config"
+import { defineConfig, envField } from "astro/config"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  integrations: [react()],
+    integrations: [react()],
+
+    env: {
+        schema: {
+            // biome-ignore lint/style/useNamingConvention: Env var
+            NOTION_WEBHOOK_SECRET: envField.string({
+                access: "secret",
+                context: "server",
+                optional: false,
+            }),
+        },
+    },
 })
---
+++ src/pages/api/add-thought.ts
@@ -0,0 +1,16 @@
+import { NOTION_WEBHOOK_SECRET } from "astro:env/server"
+import type { APIRoute } from "astro"
+
+export const POST: APIRoute = async ({ request }) => {
+    if (request.headers.get("content-type") !== "application/json")
+        return new Response("Bad Request", { status: 400 })
+
+    if (request.headers.get("secret") !== NOTION_WEBHOOK_SECRET)
+        return new Response("Unauthorized", { status: 401 })
+
+    console.log(await request.json())
+    return new Response("Good", { status: 200 })
+}
+
+export const ALL: APIRoute = () =>
+    new Response("Method Not Allowed", { status: 405 })
