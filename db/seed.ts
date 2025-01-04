5565e41caafecba1c4c864f2808236cc4030656b
Shreyans Jain <shreyans@shreyans.sh>
2025-01-04 20:10:55 +0530

    thoughts frontend
    Signed-off-by: Shreyans Jain <shreyans@shreyans.sh>

--- db/seed.ts
+++ db/seed.ts
@@ -9,7 +9,97 @@ export default async function seed() {
         },
         {
             index: 2,
-            thought: "Goodbye, World!",
+            thought: "Should giving birth be a human right?",
+            date: new Date(),
+        },
+        {
+            index: 3,
+            thought: "What is the meaning of life?",
+            date: new Date(),
+        },
+        {
+            index: 4,
+            thought: "What is the purpose of life?",
+            date: new Date(),
+        },
+        {
+            index: 5,
+            thought: "What is the purpose of death?",
+            date: new Date(),
+        },
+        {
+            index: 6,
+            thought: "What is the meaning of death?",
+            date: new Date(),
+        },
+        {
+            index: 7,
+            thought: "What is the purpose of birth?",
+            date: new Date(),
+        },
+        {
+            index: 8,
+            thought: "What is the meaning of birth?",
+            date: new Date(),
+        },
+        {
+            index: 9,
+            thought: "What is the purpose of existence?",
+            date: new Date(),
+        },
+        {
+            index: 10,
+            thought: "What is the meaning of existence?",
+            date: new Date(),
+        },
+        {
+            index: 11,
+            thought: "What is the purpose of the universe?",
+            date: new Date(),
+        },
+        {
+            index: 12,
+            thought: "What is the meaning of the universe?",
+            date: new Date(),
+        },
+        {
+            index: 13,
+            thought: "What is the purpose of the world?",
+            date: new Date(),
+        },
+        {
+            index: 14,
+            thought: "What is the meaning of the world?",
+            date: new Date(),
+        },
+        {
+            index: 15,
+            thought: "What is the purpose of the cosmos?",
+            date: new Date(),
+        },
+        {
+            index: 16,
+            thought: "What is the meaning of the cosmos?",
+            date: new Date(),
+        },
+        {
+            index: 17,
+            thought: "What is the purpose of the galaxy?",
+            date: new Date(),
+        },
+        {
+            index: 18,
+            thought: "What is the meaning of the galaxy?",
+            date: new Date(),
+        },
+        {
+            index: 19,
+            thought: "What is the purpose of the solar system?",
+            date: new Date(),
+        },
+        {
+            index: 20,
+            thought: "What is the meaning of the solar system?",
             date: new Date(),
         },
     ])
